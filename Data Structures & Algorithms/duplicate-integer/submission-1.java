9391674ef30e11dc4d3a9246262cc90f3add3fe8
12210823 <129576572+12210823@users.noreply.github.com>
2026-08-22 19:34:46 -0700

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i ++)
+        {
+            if (i + 1 < nums.length && nums[i] == nums[i + 1])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
